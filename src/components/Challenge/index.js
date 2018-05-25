98d0c6e6fb9a2fcc0f11dde088b0f356ab972e6c
Tom Horton <thomasphorton@gmail.com>
2018-05-24 22:01:13 -0700

    Saves challenge completion state with localstorage.

--- src/components/Challenge/index.js
+++ src/components/Challenge/index.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 
-function Challenge(props) {
-  function handleClick(e) {
-    console.log('clicked');
+class Challenge extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false
+    };
+
+    if (localStorage.getItem(props.challenge.id)) {
+      this.state = JSON.parse(localStorage.getItem(props.challenge.id.toString()));
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const id = this.props.challenge.id.toString();
+
+    this.setState({
+      isChecked: value
+    }, () => {
+      localStorage.setItem(id, JSON.stringify(this.state));
+    });
+
     ReactGA.event({
       category: 'challenge',
       action: 'click',
-      label: props.challenge.id,
+      label: id,
       value: 1
-    })
+    });
   }
 
-  function createMarkup() {
+  createMarkup() {
     return {
-      __html: props.challenge.text
+      __html: this.props.challenge.text
     }
   }
 
-  return (
-    <li>
-      <label>
-        <input type="checkbox" name="{props.challenge.id}" value="" onClick={handleClick} /> <span dangerouslySetInnerHTML={createMarkup()}/>
-      </label>
-    </li>
-  )
+  render() {
+    return (
+      <li>
+        <label>
+          <input
+            name={this.props.challenge.id}
+            type="checkbox"
+            checked={this.state.isChecked}
+            onChange={this.handleInputChange} />
+          <span dangerouslySetInnerHTML={this.createMarkup()}/>
+        </label>
+      </li>
+    )
+  }
+
 }
 
 export default Challenge;
